1ba6a5e25e445a4fd51d0f9698c3b3447355e09b
EvanRPavone <evanrpavone@gmail.com>
2020-11-02 10:27:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function fetchBooks() {
-
+  let f = fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    renderBooks(json)
+  });
+  return f;
 }
 
 function renderBooks(books) {
