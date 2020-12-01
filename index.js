d457314e7cf2c4fe3f60294a3ba5a37acda6a968
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-12-01 22:19:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-function fetchBooks() {
+function fetchBooks(){
   return fetch('https://anapioficeandfire.com/api/books')
     .then(resp => resp.json())
-    .then(json => renderBooks(json));
-
+    .then(json => renderBooks());
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main')
-  books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-  })
+function renderBooks(){
+  
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
